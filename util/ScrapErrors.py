a058c3115820ffe882e0f852ce551ef14841f018
iago <xxiagointer@gmail.com>
2021-12-14 09:31:32 -0300

    exception classes added

---
+++ util/ScrapErrors.py
@@ -0,0 +1,29 @@
+
+class Error(Exception):
+    """ Base class for exceptions """
+    def __init__(self, expression : str):
+        self.expression = expression
+        self.message = "Error: " + expression + " is undefined!"
+
+class BaseUndefinedError(Error):
+    def __init__(self, expression : str):
+        super().__init__(expression)
+
+class ContentTypeError(Error):
+    def __init__(self, expression : str):
+        self.expression = expression
+        self.message = "Error: " + expression + " is undefined as ContentType!"    
+
+class ContentTypeUndefinedError(Error):
+    def __init__(self, expression : str):
+        super().__init__(expression)
+
+class ParseParenthesisError(Error):
+    def __init__(self, expression : str):
+        self.expression = expression
+        self.message = "Error: parenthesis " + expression + " unmatched in search string!"    
+
+class ParseError(Error):
+    def __init__(self, expression : str):
+        self.expression = expression
+        self.message = "Error: invalid token \"" + expression + "\" in search string!"    
